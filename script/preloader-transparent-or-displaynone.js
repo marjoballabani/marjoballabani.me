cfa76d86e4f3cf0e994ef2de8c787112382e09fc
Marjo <marjo.ballabani@unicepta.com>
2024-03-07 00:16:53 +0100

    new avatar

--- script/preloader-transparent-or-displaynone.js
+++ script/preloader-transparent-or-displaynone.js
@@ -1,1 +1,7 @@
-function setPreloaderTransparentOrDisplaynone(){"internet explorer"==browserName&&browserVersion<=8?preloaderDiv.setAttribute("class","displaynone"):preloaderDiv.setAttribute("class","transparent")}var preloaderDiv=document.getElementById("preloader");setPreloaderTransparentOrDisplaynone();
+function setPreloaderTransparentOrDisplaynone() {
+  "internet explorer" == browserName && browserVersion <= 8
+    ? preloaderDiv.setAttribute("class", "displaynone")
+    : preloaderDiv.setAttribute("class", "transparent");
+}
+var preloaderDiv = document.getElementById("preloader");
+setPreloaderTransparentOrDisplaynone();
